ec2a065e3cec36ddaa3c80a5b32f3f5fb9131d91
alejandro <guatebus@gmail.com>
2013-10-25 16:04:19 +0200

    Changed site configuration from 'development' to 'production'

--- wurfl/examples/demo/inc/wurfl_config_standard.php
+++ wurfl/examples/demo/inc/wurfl_config_standard.php
@@ -4,8 +4,8 @@
  */
 
 // Enable all error logging while in development
-ini_set('display_errors', 'on');
-error_reporting(E_ALL);
+// ini_set('display_errors', 'on');
+// error_reporting(E_ALL);
 
 $wurflDir = dirname(__FILE__) . '/../../../WURFL';
 $resourcesDir = dirname(__FILE__) . '/../../resources';
