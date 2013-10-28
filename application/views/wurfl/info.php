8ea276426d06a506c213838eeaf5e5b795e9e427
alejandro <guatebus@gmail.com>
2013-10-28 13:15:58 +0100

    Added further detail to information (for clarity).

--- application/views/wurfl/info.php
+++ application/views/wurfl/info.php
@@ -3,14 +3,14 @@
 	</br><div class="content-main">
 		<div>
 			<ul>
-				<li><span class="bold-tag">ID</span>: <?php echo $requestingDevice->id; ?> </li>
-				<li><span class="bold-tag">Model Name</span>: <?php echo $requestingDevice->getCapability('model_name'); ?> </li>
-				<li><span class="bold-tag">Brand Name</span>: <?php echo $requestingDevice->getCapability('brand_name'); ?> </li>
-				</br><li><span class="bold-tag">Is Handheld Device</span>: <?php echo $this->session->userdata($this->config->item('my_sess_is_handheld')) ? 'True' : 'False' ?> </li>
-				</br><li><span class="bold-tag">Resolution Width</span>: <?php echo $requestingDevice->getCapability('resolution_width'); ?> </li>
-				<li><span class="bold-tag">Resolution Height</span>: <?php echo $requestingDevice->getCapability('resolution_height'); ?> </li>
-				</br><li><span class="bold-tag">Marketing Name</span>: <?php echo $requestingDevice->getCapability('marketing_name'); ?> </li>
-				<li><span class="bold-tag">Preferred Markup</span>: <?php echo $requestingDevice->getCapability('preferred_markup'); ?> </li>
+				<li><span class="bold-tag">Device ID</span>: <?php echo $requestingDevice->id; ?> </li>
+				<li><span class="bold-tag">Device Model Name</span>: <?php echo $requestingDevice->getCapability('model_name'); ?> </li>
+				<li><span class="bold-tag">Device Brand Name</span>: <?php echo $requestingDevice->getCapability('brand_name'); ?> </li>
+				</br><li><span class="bold-tag">Device Is Handheld</span>: <?php echo $this->session->userdata($this->config->item('my_sess_is_handheld')) ? 'True' : 'False' ?> </li>
+				</br><li><span class="bold-tag">Device Resolution Width</span>: <?php echo $requestingDevice->getCapability('resolution_width'); ?> </li>
+				<li><span class="bold-tag">Device Resolution Height</span>: <?php echo $requestingDevice->getCapability('resolution_height'); ?> </li>
+				</br><li><span class="bold-tag">Device Marketing Name</span>: <?php echo $requestingDevice->getCapability('marketing_name'); ?> </li>
+				<li><span class="bold-tag">Device Preferred Markup</span>: <?php echo $requestingDevice->getCapability('preferred_markup'); ?> </li>
 			</ul>
 		</div>
 		</br><div><span class="bold-tag">WURFL Version</span>: <?php echo $wurflInfo->version ?></div>
